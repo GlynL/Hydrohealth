3001d44c70255d4997fe3c46045f4ce2e5252192
Glyn L <glyn_92@hotmail.com>
2017-10-27 18:18:19 +0700

    added all pages & dropdown menu/page

--- hydrohealth.js
+++ hydrohealth.js
@@ -3,7 +3,11 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
-    } else {
+    } 
+    
+    
+
+    else {
         x.className = "topnav";
     }
 }
